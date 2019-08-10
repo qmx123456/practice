8b42dbde565b61390c9ec01b51cd5975dc9dc69b
qmx123456 <1639296181@qq.com>
2019-08-10 14:37:41 +0800

    003 fix 5->'Buzz'

--- src/main/java/NumberOut.java
+++ src/main/java/NumberOut.java
@@ -6,7 +6,9 @@ public class NumberOut {
 
     public String out() {
         String res = null;
-        if (num%3 == 0){
+        if (num%5==0){
+            res = "Buzz";
+        }else if (num%3 == 0){
             res = "Fizz";
         }else {
             res = Integer.toString(num);
