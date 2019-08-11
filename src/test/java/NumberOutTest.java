ff2ccba1bdbdc79988b3223a3983e96503332596
qmx123456 <1639296181@qq.com>
2019-08-11 14:43:35 +0800

    003 test 5->'Buzz'

--- src/test/java/NumberOutTest.java
+++ src/test/java/NumberOutTest.java
@@ -13,6 +13,11 @@ public class NumberOutTest {
         assertOut(3, "Fizz");
     }
 
+    @Test
+    public void should_out_Buzz_when_num_is_divisible_by_5(){
+        assertOut(5, "Buzz");
+    }
+
     private void assertOut(int i, String s) {
         NumberOut numberOut = new NumberOut(i);
         Assert.assertEquals(s, numberOut.out());
