ff000f187bedf48cb7389fdd32f5248c3c7032cc
qmx123456 <1639296181@qq.com>
2019-08-11 10:21:46 +0800

    004 refactor extract conditions

--- src/main/java/NumberOut.java
+++ src/main/java/NumberOut.java
@@ -1,20 +1,26 @@
 public class NumberOut {
     int num;
+
     public NumberOut(int i) {
         num=i;
     }
 
     public String out() {
         String res = null;
-        if (num%3 == 0 && num%5==0){
+        if (isRelatedTo(3) && isRelatedTo(5)){
             res = "FizzBuzz";
-        }else if (num%5==0){
+        }else if (isRelatedTo(5)){
             res = "Buzz";
-        }else if (num%3 == 0){
+        }else if (isRelatedTo(3)){
             res = "Fizz";
         }else {
             res = Integer.toString(num);
         }
         return res;
     }
+
+    private boolean isRelatedTo(int i) {
+        return num % i == 0;
+    }
+
 }
