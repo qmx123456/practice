e4190b528c5ded575ff6a61e8081a89a9d177e31
qmx123456 <1639296181@qq.com>
2019-08-16 15:46:13 +0800

    parserArg 001 use parser with schema

--- src/main/java/ParserArg.java
+++ src/main/java/ParserArg.java
@@ -1,2 +1,18 @@
+import java.util.List;
+
 public class ParserArg {
+
+    private final SchemaArg schemaArg;
+
+    public ParserArg() {
+        schemaArg = new SchemaArg();
+    }
+
+    public List<String> parser(String s) {
+        return schemaArg.match(s);
+    }
+
+    public SchemaArg getSchema() {
+        return schemaArg;
+    }
 }
--- src/test/java/parseTest.java
+++ src/test/java/parseTest.java
@@ -1,2 +1,16 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
 public class parseTest {
+    @Test
+    public  void should_output_schema_when_input_format_command(){
+        ParserArg parserArg = new ParserArg();
+        List<String> parser = parserArg.parser("-l -p 80 -d /usr/logs");
+        Assert.assertEquals(0,parser.size());
+        Assert.assertEquals(true,parserArg.getSchema().getLogFlag().getValue());
+        Assert.assertEquals(80,parserArg.getSchema().getPortArg().getValue());
+        Assert.assertEquals("/usr/logs",parserArg.getSchema().getDirectory().getValue());
+    }
 }
