c238c64548b6e1900b8fdd983346606b4cb2043b
qmx123456 <1639296181@qq.com>
2019-08-29 21:01:10 +0800

    main

--- src/main/java/NumberOut.java
+++ src/main/java/NumberOut.java
@@ -1,10 +1,26 @@
 public class NumberOut {
+    public static void main(String[] args) {
+        getNum(50);
+        getAll(100);
+    }
+
+    private static void getNum(int i) {
+        System.out.println(i + " = " + new NumberOut(i).out());
+    }
+
+    private static void getAll(int max) {
+        for (int i = 1; i<= max; i++){
+            getNum(i);
+        }
+    }
+
     int num;
     public NumberOut(int i) {
         num=i;
     }
 
     public String out() {
+
         String res = "";
         if (isRelatedTo(3) && isRelatedTo(5)) {
             res = "FizzBuzz";
